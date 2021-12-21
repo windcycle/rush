eae713df38ec3ee5d6cea2da5b865bac8f3823ec
windcycle <96187407+windcycle@users.noreply.github.com>
2021-12-21 06:57:25 -0800

    Update cheatdino.js

--- cheatdino.js
+++ cheatdino.js
@@ -1,5 +1,4 @@
-<!--Copy this script below!-->
 var rushUp = Runner.prototype.gameOver;
 Runner.prototype.gameOver = function{}();
-<!--Kecepatan berapa detik misalnya setSpeed(50) 50 detik!-->
 Runner.instance_.setSpeed(100);
+//asdasd
